eebc1b20e0bfed3461884b25d7ee2eb2d0761877
sj Hong <shawn95981@gmail.com>
2024-04-03 20:19:13 -0400

    initial commit

---
+++ src/main/java/io/github/seongjinhong_ca/code_rather/CodeRatherApplication.java
@@ -0,0 +1,13 @@
+package io.github.seongjinhong_ca.code_rather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeRatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeRatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/seongjinhong_ca/code_rather/CodeRatherApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seongjinhong_ca.code_rather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
